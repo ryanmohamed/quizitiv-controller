ab79e43843900ac226c246704287c46c7ce35b1b
Ryan <reyaznyc@gmail.com>
2023-04-10 02:18:31 -0400

    deploy

--- config.ru
+++ config.ru
@@ -1,3 +1,3 @@
-require_relative "config/environment"
+require_relative "./app"
 
-run Rails.application
+run Sinatra.application
---
+++ config/puma.rb
@@ -0,0 +1,13 @@
+workers Integer(2)
+threads_count = Integer(5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
